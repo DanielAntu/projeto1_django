5a7b4147e84065348d3229e64f13e5927ef9af4b
Daniel Antunes <antunesdaniel083@gmail.com>
2024-04-10 15:07:47 -0300

    first commit

---
+++ recipes/tests/test_recipe_detail_view.py
@@ -0,0 +1,28 @@
+from django.urls import reverse, resolve
+from recipes import views
+from .test_recipe_base import RecipeTestBase
+from unittest import skip
+
+class RecipeDetailViewsTest(RecipeTestBase):
+    def test_recipe_detail_view_function_is_correctI(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'pk': 1}))
+        self.assertIs(view.func.view_class, views.RecipeDetail)
+
+    def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(reverse('recipes:recipe', kwargs={'pk': 1000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_loads_the_correct_recipes(self):
+        needed_title = 'This is a detail page - It load one recipe'
+        self.make_recipe(title=needed_title)
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={'pk': 1}))
+        content = response.content.decode('utf-8')
+
+        self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipes_not_published(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'pk': recipe.id}))
+
+        self.assertEqual(response.status_code, 404)
---
+++ utils/strings.py
@@ -0,0 +1,11 @@
+def is_positive_number(value):
+    try:
+        number_string = float(value)
+    except ValueError as e:
+        return False
+    
+    return number_string > 0
+
+if __name__ == '__main__':
+    print(is_positive_number('10'))
+    print(is_positive_number('-10'))
